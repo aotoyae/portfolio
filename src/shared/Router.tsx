38b99c31dc1592a93a1b5750b05d286baa78252c
kimsohyeon <aotoyae@gmail.com>
2024-06-14 17:12:08 +0900

    feat: layout 설정

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
 import Home from '../pages/Home';
 import Project from '../pages/Project';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Project />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Project />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
