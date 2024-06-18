b2577dc03a98d25006643a42124c783d8d800fa6
kimsohyeon <aotoyae@gmail.com>
2024-06-18 18:59:21 +0900

    style: layout w 85vw 로 수정

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="w-[80vw] mx-auto bg-gray-200">
+    <main className="w-[85vw] mx-auto bg-gray-200">
       <Header />
       <article>{children}</article>
     </main>
