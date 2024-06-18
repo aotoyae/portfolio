18e31053c6ac312824c13c9c6b1574686a068fe7
kimsohyeon <aotoyae@gmail.com>
2024-06-18 18:24:54 +0900

    style: 자기소개 레이아웃 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
-      <p>
+    <main className="flex flex-col gap-20">
+      <p className="pl-[30%] mt-4">
         자기소개.. 국가는 과학기술의 혁신과 정보 및 인력의 개발을 통하여
         국민경제의 발전에 노력하여야 한다. 누구든지 체포 또는 구속을 당한 때에는
         즉시 변호인의 조력을 받을 권리를 가진다. 다만, 형사피고인이 스스로
@@ -13,8 +13,8 @@ const Home = () => {
         붙인다. 각급 선거관리위원회의 조직·직무범위 기타 필요한 사항은 법률로
         정한다.
       </p>
-      <main>
-        <article className="flex flex-col gap-4">
+      <article>
+        <section className="flex flex-col gap-4">
           <Link
             to="#"
             onClick={handleMailto}
@@ -24,9 +24,9 @@ const Home = () => {
           </Link>
           <InfoLink url="https://aotoyae.tistory.com/">blog</InfoLink>
           <InfoLink url="https://github.com/aotoyae">github</InfoLink>
-        </article>
-      </main>
-    </>
+        </section>
+      </article>
+    </main>
   );
 };
 
