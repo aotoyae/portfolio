9103193cb23359a42c560baf7a4cfb6733f39d39
kimsohyeon <aotoyae@gmail.com>
2024-06-18 17:29:05 +0900

    delete: header nav delete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <nav>
-      <ul className="flex gap-6">
-        <li>
-          <Link to="/">sohyeon kim</Link>
-        </li>
-        <li>
-          <Link to="/project">project</Link>
-        </li>
-        <li>contact</li>
-      </ul>
-    </nav>
+    <ul className="flex gap-6">
+      <li>
+        <Link to="/">sohyeon kim</Link>
+      </li>
+      <li>
+        <Link to="/project">project</Link>
+      </li>
+      <li>contact</li>
+    </ul>
   );
 };
 
