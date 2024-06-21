493ebe8af24ecbd8a1a5e93338e02fadc21e2314
kimsohyeon <aotoyae@gmail.com>
2024-06-21 17:22:45 +0900

    style: home mt 수정

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="w-[85vw] h-[100vh] mx-auto bg-gray-200">
       <Header />
-      <article className="flex justify-center h-[calc(100vh-24px)] place-items-center">
+      <article className="flex justify-center h-[calc(100%-24px)] place-items-center">
         {children}
       </article>
     </main>
