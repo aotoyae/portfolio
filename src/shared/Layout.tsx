bb19fb6fd5d0139ea84190bf56235ca009814e2e
kimsohyeon <aotoyae@gmail.com>
2024-06-21 21:38:17 +0900

    style: layout py-4

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="w-[85vw] h-[100vh] mx-auto bg-gray-200">
+    <main className="w-[85vw] py-4 h-[100vh] mx-auto bg-gray-200">
       <Header />
       <article className="flex justify-center h-[calc(100%-24px)] place-items-center">
         {children}
