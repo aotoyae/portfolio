79139fc204a29f56001eed7453851505a1f9e472
kimsohyeon <aotoyae@gmail.com>
2024-06-28 17:32:57 +0900

    style: pjt detail 컨텐츠 flex

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -25,45 +25,47 @@ const Detail = () => {
   } = data;
 
   return (
-    <main className="h-full">
+    <main className="h-full py-[3vh]">
       <h3>{name}</h3>
-      <section className="flex justify-between gap-12">
-        <article className="w-2/5 place-content-end">
-          <ul className="flex gap-3 pb-3 underline underline-offset-2">
-            {links.map((link) => (
-              <li key={link.link}>
-                <Link to={link.link} target="_blank">
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <h5 className="pb-7">{title}</h5>
-          <p className="pb-16">
-            {subTitle}
-            <br />
-            {info}
-          </p>
-          <p className="pb-2">{date}</p>
-          <p className="pb-3">{member}</p>
-          <h5>{skill}</h5>
-        </article>
-        <img src={thumbnail} className="w-3/5 h-[60vh] object-cover" />
-      </section>
-      <section></section>
-      <section>
-        <h3>트러블 슈팅</h3>
-        {troubleshooting.map((issue) => (
-          <article key={issue.title}>
-            <h5>{issue.title}</h5>
-            <p>{issue.info}</p>
+      <div className="flex flex-col h-full gap-16">
+        <section className="flex justify-between gap-12">
+          <article className="w-2/5 place-content-end">
+            <ul className="flex gap-3 pb-3 underline underline-offset-2">
+              {links.map((link) => (
+                <li key={link.link}>
+                  <Link to={link.link} target="_blank">
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <h5 className="pb-7">{title}</h5>
+            <p className="pb-16">
+              {subTitle}
+              <br />
+              {info}
+            </p>
+            <p className="pb-2">{date}</p>
+            <p className="pb-3">{member}</p>
+            <h5>{skill}</h5>
           </article>
-        ))}
-      </section>
-      <section>
-        <h3>회고</h3>
-        <p>{retrospect}</p>
-      </section>
+          <img src={thumbnail} className="w-3/5 h-[60vh] object-cover" />
+        </section>
+        <section></section>
+        <section>
+          <h3>트러블 슈팅</h3>
+          {troubleshooting.map((issue) => (
+            <article key={issue.title}>
+              <h5>{issue.title}</h5>
+              <p className="w-2/4">{issue.info}</p>
+            </article>
+          ))}
+        </section>
+        <section>
+          <h3>회고</h3>
+          <p>{retrospect}</p>
+        </section>
+      </div>
     </main>
   );
 };
