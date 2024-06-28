30f613427b6b6aa86ecf899f9956fcfe4509d1a9
kimsohyeon <aotoyae@gmail.com>
2024-06-28 17:05:48 +0900

    style: pjt info 부분 padding

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -10,14 +10,15 @@ const Detail = () => {
 
   if (!data) return <div>Data not found</div>;
 
-  const { name, thumbnail, links, title, subTitle, info } = data;
+  const { name, thumbnail, links, title, subTitle, info, date, member, skill } =
+    data;
 
   return (
     <main className="h-full">
       <h3>{name}</h3>
       <section className="flex justify-between gap-12">
-        <article className="w-2/5">
-          <ul>
+        <article className="w-2/5 place-content-end">
+          <ul className="flex gap-3 pb-3 underline underline-offset-2">
             {links.map((link) => (
               <li key={link.link}>
                 <Link to={link.link} target="_blank">
@@ -26,9 +27,15 @@ const Detail = () => {
               </li>
             ))}
           </ul>
-          <h5>{title}</h5>
-          <p>{subTitle}</p>
-          <p>{info}</p>
+          <h5 className="pb-7">{title}</h5>
+          <p className="pb-16">
+            {subTitle}
+            <br />
+            {info}
+          </p>
+          <p className="pb-2">{date}</p>
+          <p className="pb-3">{member}</p>
+          <h5>{skill}</h5>
         </article>
         <img src={thumbnail} className="w-3/5 h-[60vh] object-cover" />
       </section>
