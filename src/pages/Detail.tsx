b59b098920162a4b6da9dbc10f8ffae34b305418
kimsohyeon <aotoyae@gmail.com>
2024-07-07 12:37:45 +0900

    style: watchapedia 보더 컬러 변경

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -65,7 +65,7 @@ const Detail = () => {
                 <img
                   src={issue.image}
                   alt="기능 이미지"
-                  className={`object-contain w-full h-[30vh] border-solid border-y ${id == 'mmeasy' ? 'border-[#2a83ee]' : 'border-[#dcbaa3]'}`}
+                  className={`object-contain w-full h-[30vh] border-solid border-y ${id == 'mmeasy' ? 'border-[#2a83ee]' : id == 'gabaedo' ? 'border-[#dcbaa3]' : 'border-[#ff90b4]'}`}
                 />
                 <h5 className="pt-5 pb-2">{issue.title}</h5>
                 {issue.info &&
