a7175b3487801a7c44023e77f8637fa6903d2291
kimsohyeon <aotoyae@gmail.com>
2025-03-03 22:50:25 +0900

    fix: 이메일 수정

--- src/utils/handleMailto.ts
+++ src/utils/handleMailto.ts
@@ -3,5 +3,5 @@ export const handleMailto = (
 ) => {
   // 제네릭 타입 파라미터(처음은 이벤트가 발생하는 요소 <a></a>, 다음은 이벤트의 타입)
   e.preventDefault();
-  window.location.href = 'mailto:aotoyae@gamil.com';
+  window.location.href = 'mailto:aotoyae@gmail.com';
 };
